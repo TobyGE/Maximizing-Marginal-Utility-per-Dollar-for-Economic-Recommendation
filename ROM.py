47411e63aa35e345c1fc65fa75c2746f3e0aff7c
Sean <xusy.1024@gmail.com>
2019-04-19 14:52:00 -0400

    add comment for ROM

--- ROM.py
+++ ROM.py
@@ -5,6 +5,13 @@ import torch.nn as nn
 class ROM(nn.Module):
 	"""docstring for ROM"""
 	def __init__(self, userLen, itemLen, params):
+		"""
+		- userLen: the number of users
+		- itemLen: the number of items
+		- params: the parameters used for constructing model
+			- l_size: latent dimension size
+			- gpu: True/False, whether using GPU
+		"""
 		super(ROM, self).__init__()
 		self.userNum = userLen
 		self.itemNum = itemLen
