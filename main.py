d48afbd9f3f28a33c20cf5a5b2e914448a9e40ff
Sean <xusy.1024@gmail.com>
2019-04-20 03:39:46 -0400

    rest code

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import torch.nn as nn
 import torch.optim as opt
 from tqdm import tqdm
 from heapq import heappush, heappop
+from torch.utils.data import Dataset, DataLoader
 
 import evaluation
 import data_loader
-import model
+from model import MF, MUD
 
 def main(category):
 	params = dict()
 	params['lr'] = 0.001
 	params['batch_size'] = 16
-	params['epoch_limit'] = 10
-	params['weight_decay'] = 1
+	params['epoch_limit'] = 1
+	params['w_decay'] = 1
 	params['negNum'] = 1000
+	params['epsilon'] = 1e-4
 
 	params['l_size'] = 32
 	params['gpu']=False
@@ -23,9 +25,200 @@ def main(category):
 	train, val, test = data_loader.read_data(category)
 	item_price = data_loader.get_price(category)
 	item_related = data_loader.read_related(category)
-	trainset = data_loader.TransactionData(train, item_related, item_price)
+	distribution = data_loader.get_distribution(category)
+	trainset = data_loader.TransactionData(train, item_related, \
+				item_price, distribution)
 	valset = data_loader.UserTransactionData(val, item_price, \
 				trainset.itemNum, trainset.userHist)
+	testset = data_loader.UserTransactionData(test, item_price, \
+				trainset.itemNum, trainset.userHist)
+
+	Rating = MF(userLen = trainset.userNum, itemLen = trainset.itemNum,\
+			 params = params)
+	optimizer_r = opt.SGD(Rating.parameters(), lr = params['lr'], \
+			weight_decay = params['w_decay'])
+	
+	criterion_rating = nn.MSELoss(reduction = 'sum')
+	criterion_risk = nn.MSELoss(reduction = 'sum')
+	criterion_MUD = nn.LogSoftmax()
+
+	trainset.set_negN(4)
+	trainLoader = DataLoader(trainset, batch_size = params['batch_size'], \
+							shuffle = True, num_workers = 0)
+	valset.set_negN(1000)
+	valLoader = DataLoader(valset, batch_size = 1, \
+							shuffle = True, num_workers = 0)
+	testset.set_negN(1000)
+	testLoader = DataLoader(testset, batch_size = 1, \
+							shuffle = True, num_workers = 0)
+
+	epsilon = params['epsilon']
+	epoch = 1
+	error = np.float('inf')
+
+	print("starting pretrain the rating matrix...")
+	while epoch < params['epoch_limit']:
+		runningLoss = []
+		epoch += 1
+		print("Epoch " + str(epoch) + " training...")
+		L = len(trainLoader.dataset)
+		pbar = tqdm(total=L)
+		for i, batchData in enumerate(trainLoader):
+			optimizer_r.zero_grad()
+			# get input
+			users = torch.LongTensor(batchData['user']).to(Rating.device)
+			items = torch.LongTensor(batchData['item']).to(Rating.device)
+			pre_r = Rating.forward(users, items)
+			r = torch.FloatTensor(batchData['rating']).to(Rating.device)
+
+			loss = criterion_rating(pre_r, r)
+			loss.backward()
+			optimizer_r.step()
+			runningLoss.append(loss.item())
+
+			pbar.update(users.shape[0])
+		pbar.close()
+		meanMSE = np.mean(np.array(runningLoss))
+		improvement = np.abs(error - meanMSE)
+		error = meanMSE
+		if improvement <= epsilon:
+			print('pre-train stop early')
+			break
+
+	model = MUD(userLen = trainset.userNum, itemLen = trainset.itemNum,\
+			 distribution = distribution, item_price = item_price, \
+			 RMF = Rating, params = params)
+	optimizer = opt.SGD(model.parameters(), lr = params['lr'], \
+			weight_decay = params['w_decay'])
+
+	epsilon = params['epsilon']
+	epoch = 0
+	error = np.float('inf')
+
+	trainErrorList = []
+	valErrorList = []
+	valHistory = []
+	explodeTempreture = 3
+	convergenceTempreture = 3
+	print("starting ROM model")
+	while epoch < params['epoch_limit']:
+		epoch += 1
+		print("Epoch " + str(epoch) + " training...")
+		L = len(trainLoader.dataset)
+		pbar = tqdm(total = L)
+		for i, batchData in enumerate(trainLoader):
+			optimizer.zero_grad()
+			# get input
+			users = torch.LongTensor(batchData['user']).to(model.device)
+			# print(users.shape)
+			items = torch.LongTensor(batchData['item']).to(model.device)
+			# print(items.shape)
+			negItems = torch.LongTensor(batchData['negItem']).to(model.device)
+			# print(negItems.shape)
+			nusers = users.view(-1,1).expand(users.shape[0], negItems.shape[1])
+
+			pOut = model.forward(users,items)
+			nOut = model.forward(nusers, negItems)
+			totalOut = torch.cat((pOut,nOut),1)
+
+			loss = criterion_MUD(totalOut)[:,0]
+			loss.backward()
+			optimizer.step()
+			pbar.update(users.shape[0])
+		pbar.close()
+		print("Epoch " + str(epoch) + " training risk...")
+		pbar = tqdm(total = L)
+		for i, batchData in enumerate(trainLoader):
+			optimizer.zero_grad()
+			users = torch.LongTensor(batchData['user']).to(model.device)
+			items = torch.LongTensor(batchData['user']).to(model.device)
+
+			eu = model.EU(users,items)
+			ue = model.UE(users,items)
+
+			loss = criterion_risk(ue, eu)
+			loss.backward()
+			optimizer.step()
+			pbar.update(users.shape[0])
+		pbar.close()
+
+		#validation
+		print("Epoch " + str(epoch) + " validating...")
+		L = len(valLoader.datset)
+		pbar = tqdm(total = L)
+		model.eval()
+		with torch.no_grad():
+			scoreDict = dict()
+			for i, batchData in enumerate(valLoader):
+				user = torch.LongTensor(batchData['user']).to(model.device)
+				posItems = torch.LongTensor(batchData['posItem']).to(model.device)
+				negItems = torch.LongTensor(batchData['negItem']).to(model.device)
+				budget = torch.FloatTensor(batchData['budget']).to(model.device)
+				posPrices = torch.FloatTensor(batchData['posPrice']).to(model.device)
+				negPrices = torch.FloatTensor(batchData['negPrice']).to(model.device)
+
+				items = torch.cat((posItems, negItems),1).view(-1)
+				prices = torch.cat((posPrices, negPrices),1).view(-1)
+				users = user.expand(items.shape[0])
+
+				out = model.forward(users,items)
+				scoreHeap = list()
+				for j in range(out.shape[0]):
+					gt = False
+					if j < posItems.shape[1]:
+						gt = True
+					if prices[j] > budget:
+						continue
+					heappush(scoreHeap, (1 - out[j].cpu().numpy(), (0 + items[j].cpu().numpy(), gt)))
+				scores = list()
+				candidate = len(scoreHeap)
+				for k in range(candidate):
+					scores.append(heappop(scoreHeap))
+				pbar.update(1)
+				scoreDict[user[0]] = (scores, posItems.shape[1])
+		pbar.close()
+
+		valHistory.append(evaluation.ranking_performance(scoreDict,100))
+		valError = 1 - valHistory[-1]["avg_ndcg"][0]
+		valErrorList.append(valError)
+		improvement = np.abs(error - valError)
+		error = valError
+		if improvement < epsilon:
+			break
+
+		# test
+		print("starting test...")
+		with torch.no_grad():
+			scoreDict = dict()
+			for i, batchData in enumerate(testLoader):
+				user = torch.LongTensor(batchData['user']).to(model.device)
+				posItems = torch.LongTensor(batchData['posItem']).to(model.device)
+				negItems = torch.LongTensor(batchData['negItem']).to(model.device)
+				budget = torch.FloatTensor(batchData['budget']).to(model.device)
+				posPrices = torch.FloatTensor(batchData['posPrice']).to(model.device)
+				negPrices = torch.FloatTensor(batchData['negPrice']).to(model.device)
+
+				items = torch.cat((posItems, negItems),1).view(-1)
+				prices = torch.cat((posPrices, negPrices),1).view(-1)
+				users = user.expand(items.shape[0])
+
+				out = model.forward(users,items)
+				scoreHeap = list()
+				for j in range(out.shape[0]):
+					gt = False
+					if j < posItems.shape[1]:
+						gt = True
+					if prices[j] > budget:
+						continue
+					heappush(scoreHeap, (1 - out[j].cpu().numpy(), (0 + items[j].cpu().numpy(), gt)))
+				scores = list()
+				candidate = len(scoreHeap)
+				for k in range(candidate):
+					scores.append(heappop(scoreHeap))
+				pbar.update(1)
+				scoreDict[user[0]] = (scores, posItems.shape[1])
+		pbar.close()
+		testResult = evaluation.ranking_performance(scoreDict,100)
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -44,12 +44,102 @@ class MF(nn.Module):
         self.itemEmbed = nn.Embedding(itemLen, l_size)
     
     def forward(self, users, items):
-        gB = self.globalBias.weight.data.expand(users.shape[0],1)
+        uE = self.uEmbed(users)
+        uB = self.uBias(users)
+        iE = self.itemEmbed(items)
+        iB = self.itemBias(items)
+        try: 
+            gB = self.globalBias.weight.data.expand(users.shape[0],users.shape[1])
+            score = gB + uB + iB + torch.mul(uE, iE).sum(2)
+        except:
+            gB = self.globalBias.weight.data.expand(users.shape[0],1)
+            score = gB + uB + iB + torch.mul(uE, iE).sum(1).view(-1,1)
+        return score
+
+class MUD(nn.Module):
+    """docstring for MUD"""
+    def __init__(self, userLen, itemLen, distribution, item_price, RMF, params):
+        super(MUD, self).__init__()
+        self.userNum = userLen
+        self.itemNum = itemLen
+        self.distribution = torch.tensor(distribution).to(torch.float)
+        self.price = torch.tensor(item_price).to(torch.float)
+        self.rating = RMF
+        self.params = params
+        if 'gpu' in params and params['gpu'] == True:
+            self.device = 'cuda'
+        else:
+            self.device = 'cpu'
+        l_size = params['l_size']
+
+        self.gBias = nn.Embedding(1,1)
+        self.uBias = nn.Embedding(userLen,1)
+        self.itemBias = nn.Embedding(itemLen,1)
+        self.uEmbed = nn.Embedding(userLen, l_size)
+        self.itemEmbed = nn.Embedding(itemLen, l_size) 
+
+    def forward(self, users, items):
+        uE = self.uEmbed(users)
+        uB = self.uBias(users)
+        iE = self.itemEmbed(items)
+        iB = self.itemBias(items)
+        print("size")
+        
+        # print(uE.shape)
+        # print(uB.shape)
+        # print(iE.shape)
+        # print(iB.shape)
+        try:
+            gB = self.gBias.weight.data.expand(users.shape[0],users.shape[1])
+            alpha = gB + uB + iB + torch.mul(uE, iE).sum(2)
+        except:
+            gB = self.gBias.weight.data.expand(users.shape[0],1)
+            print(torch.mul(uE,iE).sum(1).shape)
+            alpha = gB + uB + iB + torch.mul(uE, iE).sum(1).view(-1,1)
+        with torch.no_grad():
+            r = self.rating.forward(users, items)
+            tanh_r = torch.tanh(r).view(-1,1)
+        price = self.price[items]
+        print(gB.shape)
+        print("alpha")
+        print(alpha.shape)
+        print(price.shape)
+        u = torch.mul(alpha, tanh_r)
+        print(u.shape)
+        out = 0.5 * torch.div(u, torch.sigmoid(price))
+        print("out")
+        print(out.shape)
+        return out
+
+    def EU(self, users, items):
+        gB = self.gBias.weight.data.expand(users.shape[0],1)
         uE = self.uEmbed(users)
         uB = self.uBias(users)
         iE = self.itemEmbed(items)
         iB = self.itemBias(items)
         
-        score = gB + uB + iB + torch.mul(uE, iE).sum(1).view(-1,1) 
-        return score
+        alpha = gB + uB + iB + torch.mul(uE, iE).sum(1).view(-1,1).expand(users.shape[0],5)
+        distribution = self.distribution[items]
+        rating = torch.tensor([1,2,3,4,5]).expand(users.shape[0],5)
+        tanh_r = torch.tanh(rating)
+        U = torch.log(2) * torch.mul(alpha, tanh_r)
+        EU = torch.mul(distribution, U).sum(1)
+        return EU
+
+    def UE(self, users, items):
+        gB = self.gBias.weight.data.expand(users.shape[0],1)
+        uE = self.uEmbed(users)
+        uB = self.uBias(users)
+        iE = self.itemEmbed(items)
+        iB = self.itemBias(items)
+        
+        alpha = gB + uB + iB + torch.mul(uE, iE).sum(1).view(-1,1)
+        distribution = self.distribution[items]
+        rating = torch.tensor([1,2,3,4,5]).expand(users.shape[0],5)
+        r_bar = torch.mul(distribution, rating).sum(1)
+        tanh_r_bar = torch.tanh(r_bar)
+        UE = torch.log(2) * torch.mul(alpha, tanh_r_bar)
+        return UE
+
+        
         
